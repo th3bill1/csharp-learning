7987c4d058da011fd42d4563eebeb1a9d5d2d62a
Wojciech Wójcik <wojciech.wojcik@k-net.pl>
2023-11-16 14:53:32 +0100

    upgraded

--- c#-learning/Lab6PL/Set.cs
+++ c#-learning/Lab6PL/Set.cs
@@ -6,12 +6,12 @@ namespace csharplearning.Lab6PL
     {
         private int[] values;
         public Set(params int[] _values) => values = _values.Distinct().ToArray();
-        public override string ToString() => values.Length>0 ? $"{{{string.Join(",", values)}}}" : "{}";
+        public override string ToString() => GetElementsArrayCapacity() > 0 ? $"{{{string.Join(",", values)}}}" : "{}";
         public int GetElementsArrayCapacity() => values.Length;
         public static Set operator +(Set a, Set b) => new((from n in a.values.Union(b.values) select n).ToArray());
         public static Set operator -(Set a, Set b) => new((from n in a.values where !b.values.Contains(n) select n).ToArray());
-        public static bool operator true(Set a) => a.values.Length > 0;
-        public static bool operator false(Set a) => a.values.Length == 0;
+        public static bool operator true(Set a) => a.GetElementsArrayCapacity() > 0;
+        public static bool operator false(Set a) => a.GetElementsArrayCapacity() == 0;
         public static Set operator &(Set a, Set b) => new((from n in a.values where b.values.Contains(n) select n).ToArray());
         public static Set operator |(Set a, Set b) => a + b;
         public static bool operator ==(Set a, Set b) => a.values.All(b.values.Contains) && b.values.All(a.values.Contains);
